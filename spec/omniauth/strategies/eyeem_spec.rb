f9fc59981ab6c8797b8732297af5f1367e3d7952
Varek <4ndr3r13ck@googlemail.com>
2013-04-18 00:22:41 +0200

    fix specs

--- spec/omniauth/strategies/eyeem_spec.rb
+++ spec/omniauth/strategies/eyeem_spec.rb
@@ -8,21 +8,21 @@ describe OmniAuth::Strategies::EyeEm do
 
   context "general" do
     it "should be called appnet" do
-      subject.options.name.should eq('appnet')
+      subject.options.name.should eq('eyeem')
     end
   end
 
   context "endpoints" do
     it "has correct site" do
-      subject.options.client_options.site.should eq("https://alpha-api.app.net")
+      subject.options.client_options.site.should eq("https://www.eyeem.com/api/v2")
     end
 
     it "has correct authorize_url" do
-      subject.options.client_options.authorize_url.should eq("https://alpha.app.net/oauth/authenticate")
+      subject.options.client_options.authorize_url.should eq("http://www.eyeem.com/oauth/authorize")
     end
 
     it "has correct token_url" do
-      subject.options.client_options.token_url.should eq("https://alpha.app.net/oauth/access_token")
+      subject.options.client_options.token_url.should eq("http://www.eyeem.com/api/v2/oauth/token")
     end
   end
 
